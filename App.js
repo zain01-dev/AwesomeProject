ef34a2c6b14b5566124902c81ef6d65b215f88f2
zain01-dev <70877665+zain01-dev@users.noreply.github.com>
2021-06-13 23:26:02 +0500

    Changes related to functional component
    Changes related to functional component

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -14,8 +14,10 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  Button,
   useColorScheme,
   View,
+  TextInput,
 } from 'react-native';
 
 import {
@@ -26,87 +28,33 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+
+
+const App = () => {
+ const greeting = 'Hello Function Component!';
+ return(<View>
+       <Headline value={greeting} />
+       <ChangeName/>
+ </View>
+ );
+};
+
+function Headline(props) {
+  return <Text>{props.value}</Text>;
+}
+
+function ChangeName() {
+  const [name, setName] = useState("zain");
+  // Similar to componentDidMount and componentDidUpdate: //replicate life cycle
+  useEffect(() => {
+      console.log("useEffect");
+  });
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text>My name is {name} </Text>
+      <Button  title="Change Name by press me" onPress={() =>{setName("zaini")} }   />
     </View>
   );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
+}
 export default App;
--- android/app/src/main/java/com/functioncomponent/MainActivity.java
+++ android/app/src/main/java/com/functioncomponent/MainActivity.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.functioncomponent;
 
 import com.facebook.react.ReactActivity;
 
