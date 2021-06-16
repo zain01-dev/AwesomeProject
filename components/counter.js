d2da8c84f3b5010bae167c84d89d2dfa4d1ac145
zain01-dev <70877665+zain01-dev@users.noreply.github.com>
2021-06-16 14:01:51 +0500

    Fetch and Axios changes
    Fetch and Axios changes completed

--- components/counter.js
+++ components/counter.js
@@ -3,13 +3,46 @@ import { Container, Content, Text, Card, Header, Body, Button, Title, CardItem }
 import { increment, decrement } from '../actions/index.js';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import {StyleSheet,View,FlatList,ActivityIndicator} from 'react-native';
 
 class Counter extends Component{
+    constructor(){
+        super();
+        this.state={
+            isLoading : true,
+            dataSource:[]
+        }
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/posts').then((response)=>response.json())
+        .then((responseJson)=>{
+            this.setState({
+                isLoading : false,
+                dataSource: responseJson
+            })
+        })
+    }
   render(){
-    console.log(this.props.count);
+      if (this.state.isLoading){
+          return(
+             <View style = {styles.container} >
+              <ActivityIndicator size="large" animating/>
+            </View>
+          )
+      }else{
+console.log(this.props.count);
     return(
       <Container>
-        <Header>
+       
+         <Text>Scroll the Below list from Server (Fetch)</Text>
+         <Text></Text>
+          <View style = {styles.container} >
+          <FlatList 
+            data = {this.state.dataSource}
+            renderItem={({item})=><Text>{item.title}</Text>}
+          />
+          </View>
+           <Header>
           <Body>
             <Title>Counter</Title>
           </Body>
@@ -31,8 +64,17 @@ class Counter extends Component{
         </Content>
       </Container>
     );
+    }
   }
 }
+const styles = StyleSheet.create({
+    container:{
+        flex : 1,
+        justifyContent:'center',
+        alignItems: 'center',
+        backgroundColor:'#F5FCFF'
+    }
+});
 function mapStateToProps(state){
   return{
     count : state.count
--- components/counterPlus.js
+++ components/counterPlus.js
@@ -2,9 +2,41 @@ import React, { Component } from 'react';
 import { Container, Content, Text, Card, Header, Body, Button, Title, CardItem } from 'native-base';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-
+import axios from 'axios'
+import {SafeAreaView,StyleSheet,View,FlatList,ActivityIndicator} from 'react-native';
 class CounterPlus extends Component{
-  render(){
+
+ constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            axiosData: []
+        };
+    }
+
+componentDidMount() {
+     this.setState({
+            loading: true,
+        })
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then(response => {
+                console.log('getting data from axios', response.data);
+                this.setState({ loading: false,  axiosData: response.data });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+}
+render(){
+       const { isLoading, axiosData } = this.state;
+       if (this.state.isLoading){
+          return(
+             <View style = {styles.container} >
+              <ActivityIndicator size="large" animating/>
+            </View>
+          )
+      }
+      else{
     console.log("Plus"+this.props.count+1);
     return(
       <Container>
@@ -22,10 +54,45 @@ class CounterPlus extends Component{
             </CardItem>
           </Card>
         </Content>
+        <Text>Scroll below Grid from server (axios)</Text>
+        <Text>      </Text>
+         <SafeAreaView style={Mystyles.container}>
+        <FlatList 
+            data = {this.state.axiosData}
+             renderItem={({ item }) => (
+                <View style={{ flex: 1, flexDirection: 'column', margin: 1 }}>
+                <Text style={Mystyles.imageThumbnail}>{item.id}</Text>
+                </View>
+        )}
+        //Setting the number of column
+        numColumns={3}
+        keyExtractor={(item, index) => index} />
+       </SafeAreaView>
       </Container>
     );
+    }    
   }
 }
+const Mystyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+   imageThumbnail: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100,
+  },
+});
+const styles = StyleSheet.create({
+    container:{
+        flex : 1,
+        justifyContent:'center',
+        alignItems: 'center',
+        backgroundColor:'#F5FCFF'
+    }
+});
 function mapStateToProps(state){
   return{
     count : state.count
